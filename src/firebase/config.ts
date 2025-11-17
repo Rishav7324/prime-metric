87671c543bcd9ca75de0b52ff6275831d9e97796
Zylos Ai <info.zylosai@gmail.com>
2025-11-17 08:27:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3474534710-e8def",
+  "appId": "1:409247733824:web:9c084b74f2d8973e7be7a5",
+  "apiKey": "AIzaSyBuX9BE-euyoz310DjFA6H2u-9-5vuWcnU",
+  "authDomain": "studio-3474534710-e8def.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "409247733824"
+};
