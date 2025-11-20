825d32f4a6beced39706a9a8e3e6c603571abe59
Zylos Ai <info.zylosai@gmail.com>
2025-11-20 09:25:25 +0000

    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsby

--- src/components/CalculatorLayout.tsx
+++ src/components/CalculatorLayout.tsx
@@ -1,6 +1,7 @@
 
 import Head from "next/head";
 import { Metadata } from 'next';
+import AdBanner from "./AdBanner";
 
 type CalculatorLayoutProps = {
   title: string;
@@ -60,6 +61,7 @@ const CalculatorLayout = ({
             )}
           </div>
           <div className="mt-12">{children}</div>
+          <AdBanner />
         </div>
       </main>
     </>
